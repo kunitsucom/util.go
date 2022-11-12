4a5d0c8fadf476ea95a216bfcab0ff36fd1a52b6
ginokent <29125616+ginokent@users.noreply.github.com>
2022-11-12 17:29:29 +0900

    feat: add osz.ReadlinkAndReadFile func

--- os/os.go
+++ os/os.go
@@ -29,3 +29,16 @@ func CheckDir(path string) error {
 
 	return ErrPathIsNotDirectory
 }
+
+func ReadlinkAndReadFile(path string) (resolved string, bytes []byte, err error) {
+	if p, err := os.Readlink(path); errors.Is(err, nil) {
+		path = p
+	}
+
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return "", nil, fmt.Errorf("os.ReadFile: %w", err)
+	}
+
+	return path, b, nil
+}
