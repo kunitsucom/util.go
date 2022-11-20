1595033eaece25b8e55d33cf5f3d36471837bfea
ginokent <29125616+ginokent@users.noreply.github.com>
2022-11-21 02:36:55 +0900

    feat: add jwk package

---
+++ pkg/jwk/jwk.go
@@ -0,0 +1,120 @@
+package jwk
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/kunitsuinc/util.go/pkg/net/http/urlcache"
+)
+
+// ref. JSON Web Key (JWK) https://www.rfc-editor.org/rfc/rfc7517
+
+type JwksURI = string
+
+// ref. JWK Set Format https://www.rfc-editor.org/rfc/rfc7517#section-5
+// ref. https://openid-foundation-japan.github.io/rfc7517.ja.html#JWKSet
+type JWKSet struct { //nolint:revive
+	// ref. https://www.rfc-editor.org/rfc/rfc7517#section-5.1
+	Keys []*JSONWebKey `json:"keys"`
+}
+
+// ref. JSON Web Key (JWK) Format https://www.rfc-editor.org/rfc/rfc7517#section-4
+// ref. https://openid-foundation-japan.github.io/rfc7517.ja.html#JWKFormat
+type JSONWebKey struct {
+	// ref. https://www.rfc-editor.org/rfc/rfc7517#section-4.1
+	KeyType string `json:"kty"`
+	// ref. https://www.rfc-editor.org/rfc/rfc7517#section-4.2
+	PublicKeyUse string `json:"use,omitempty"`
+	// ref. https://www.rfc-editor.org/rfc/rfc7517#section-4.3
+	KeyOperations string `json:"key_ops,omitempty"` //nolint:tagliatelle
+	// ref. https://www.rfc-editor.org/rfc/rfc7517#section-4.4
+	Algorithm string `json:"alg,omitempty"`
+	// ref. https://www.rfc-editor.org/rfc/rfc7517#section-4.5
+	KeyID string `json:"kid,omitempty"`
+	// ref. https://www.rfc-editor.org/rfc/rfc7517#section-4.6
+	X509URL string `json:"x5u,omitempty"`
+	// ref. https://www.rfc-editor.org/rfc/rfc7517#section-4.7
+	X509CertificateChain string `json:"x5c,omitempty"`
+	// ref. https://www.rfc-editor.org/rfc/rfc7517#section-4.8
+	X509CertificateSHA1Thumbprint string `json:"x5t,omitempty"`
+	// ref. https://www.rfc-editor.org/rfc/rfc7517#section-4.9
+	X509CertificateSHA256Thumbprint string `json:"x5t#S256,omitempty"` //nolint:tagliatelle
+
+	// RSA
+	// ref. https://www.rfc-editor.org/rfc/rfc7517#section-9.3
+
+	// N is modulus
+	N string `json:"n,omitempty"`
+	// E is public exponent
+	E string `json:"e,omitempty"`
+	// D is private exponent
+	D string `json:"d,omitempty"`
+}
+
+type Client struct { //nolint:revive
+	urlcache *urlcache.Client[*JWKSet]
+}
+
+func NewClient(opts ...NewOption) *Client {
+	d := &Client{
+		urlcache: urlcache.NewClient[*JWKSet](http.DefaultClient),
+	}
+
+	for _, opt := range opts {
+		opt(d)
+	}
+
+	return d
+}
+
+type NewOption func(*Client)
+
+func WithURLCacheClient(client *urlcache.Client[*JWKSet]) NewOption {
+	return func(d *Client) {
+		d.urlcache = client
+	}
+}
+
+func (d *Client) GetJWKSet(ctx context.Context, jwksURI JwksURI) (*JWKSet, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, jwksURI, nil)
+	if err != nil {
+		return nil, fmt.Errorf("http.NewRequestWithContext: %w", err)
+	}
+
+	resp, err := d.urlcache.Do(req, func(resp *http.Response) bool { return 200 <= resp.StatusCode && resp.StatusCode < 300 }, func(resp *http.Response) (*JWKSet, error) {
+		r := new(JWKSet)
+		if err := json.NewDecoder(resp.Body).Decode(r); err != nil {
+			return nil, fmt.Errorf("(*json.Decoder).Decode(*discovery.JWKSet): %w", err)
+		}
+		return r, nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("(*urlcache.Client).Do: %w", err)
+	}
+
+	return resp, nil
+}
+
+//nolint:gochecknoglobals
+var (
+	Default = NewClient()
+)
+
+func GetJWKSet(ctx context.Context, jwksURI JwksURI) (*JWKSet, error) {
+	return Default.GetJWKSet(ctx, jwksURI)
+}
+
+var ErrKidNotFound = errors.New("jwk: kid not found in jwks_uri")
+
+func (jwks *JWKSet) GetJSONWebKey(kid string) (*JSONWebKey, error) {
+	for _, jwk := range jwks.Keys {
+		if jwk.KeyID == kid {
+			return jwk, nil
+		}
+	}
+
+	return nil, ErrKidNotFound
+}
---
+++ pkg/jwk/jwk_test.go
@@ -0,0 +1,170 @@
+package jwk_test
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/kunitsuinc/util.go/pkg/jwk"
+	"github.com/kunitsuinc/util.go/pkg/net/http/urlcache"
+)
+
+func TestJwksURL(t *testing.T) {
+	t.Parallel()
+	url := "https://www.googleapis.com/oauth2/v3/certs"
+
+	t.Run("check("+url+")", func(t *testing.T) {
+		t.Parallel()
+		r, err := http.Get(url)
+		if err != nil {
+			t.Logf("🤔: http.Get: %v", err)
+			return
+		}
+		defer r.Body.Close()
+		buf := bytes.NewBuffer(nil)
+		if _, err := io.Copy(buf, r.Body); err != nil {
+			t.Logf("🤔: io.Copy: %v", err)
+			return
+		}
+		t.Logf("✅: %s:\n"+buf.String(), url)
+	})
+}
+
+func TestClient_GetJSONWebKey(t *testing.T) {
+	t.Parallel()
+
+	mux := http.NewServeMux()
+	testKid := "testKid"
+	mux.HandleFunc("/certs", func(w http.ResponseWriter, r *http.Request) {
+		const format = `{"keys":[{"kty":"RSA","kid":"%s","n":"%s"},{"alg":"RS256","kty":"RSA","n":"z8PS6saDU3h5ZbQb3Lwl_Arwgu65ECMi79KUlzx4tqk8bgxtaaHcqyvWqVdsA9H6Q2ZtQhBZivqV4Jg0HoPHcEwv46SEziFQNR2LH86e-WIDI5pk2NKg_9cFMee9Mz7f_NSQJ3uyD1pu86bdUTYhCw57DbEVDOuubClNMUV456dWx7dx5W4kdcQe63vGg9LXQ-9PPz9AL-0ZKr8eQEHp4KRfRUfngjqjYBMTFuuo38l94KR99B04Z-FboGnqYLgNxctwZ9eXbCerb9bV5-Q9Gb3zoo0x1h90tFdgmC2ZU1xcIIjHmFqJ29mSDZHYAAYtMNAeWreK4gqWJunc9o0vpQ","use":"sig","kid":"713fd68c966e29380981edc0164a2f6c06c5702a","e":"AQAB"},{"kty":"RSA","e":"AQAB","alg":"RS256","use":"sig","kid":"27b86dc6938dc327b204333a250ebb43b32e4b3c","n":"1X7rNtYVglDjBJgsBOSv7C6MYU6Mv-yraGOp_AGs777c2UcVGj88dBe9KihGicQ3LqU8Vf5fVhPixVy0GFBS7mJt3qJryyBpmG7sChnJQBwJmZEffZUl_rLtwGli8chbZj_Fpgjd-7t74VQJmn2SYkFqHNB3vrW_I8zmwn7_Enn4N84d4dP5R9UChUSLhuPNKaKA-a4vtTKy1LNoZpbr6LG1_QaWGDKNhgPWR-6l5fmBdaXtUgDmPFwdQZuiBUDfnPQ7t1lSUD2PJMnG3M9DKG5gqpSk1L1AlWsxntideNsKWIviZ5PhCpmzEComWNtFtFrzfAWQvLkBbgb0pwWp5w"}]}`
+		_, _ = w.Write([]byte(fmt.Sprintf(format, testKid, time.Now().Format(time.RFC3339Nano))))
+	})
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte(""))
+	})
+	s := httptest.NewServer(mux)
+	jwksURI := fmt.Sprintf("http://%s/certs", s.Listener.Addr().String())
+	rootURI := fmt.Sprintf("http://%s", s.Listener.Addr().String())
+
+	t.Run("success()", func(t *testing.T) {
+		t.Parallel()
+
+		c := jwk.NewClient(jwk.WithURLCacheClient(urlcache.NewClient[*jwk.JWKSet](http.DefaultClient)))
+		jwks1, err := c.GetJWKSet(context.Background(), jwksURI)
+		if err != nil {
+			t.Errorf("err != nil: %v", err)
+		}
+		jwk1, err := jwks1.GetJSONWebKey(testKid)
+		if err != nil {
+			t.Errorf("err != nil: %v", err)
+		}
+		first := jwk1.N
+
+		jwks2, err := c.GetJWKSet(context.Background(), jwksURI)
+		if err != nil {
+			t.Errorf("err != nil: %v", err)
+		}
+		jwk2, err := jwks2.GetJSONWebKey(testKid)
+		if err != nil {
+			t.Errorf("err != nil: %v", err)
+		}
+		cached := jwk2.N
+
+		if first != cached {
+			t.Errorf("first != cached: %v != %v", first, cached)
+		}
+	})
+
+	t.Run("failure(req)", func(t *testing.T) {
+		t.Parallel()
+
+		c := jwk.NewClient()
+		_, err := c.GetJWKSet(context.Background(), "http://%%")
+		if err == nil {
+			t.Errorf("err == nil")
+		}
+		expect := `invalid URL escape "%%"`
+		if !strings.Contains(err.Error(), expect) {
+			t.Errorf("expect != err: (%v) != (%v)", expect, err)
+		}
+	})
+
+	t.Run("failure(*json.Decoder)", func(t *testing.T) {
+		t.Parallel()
+
+		c := jwk.NewClient()
+		_, err := c.GetJWKSet(context.Background(), rootURI)
+		if err == nil {
+			t.Errorf("err == nil")
+		}
+		expect := io.EOF.Error()
+		if !strings.Contains(err.Error(), expect) {
+			t.Errorf("expect != err: (%v) != (%v)", expect, err)
+		}
+	})
+
+	t.Run("failure(ctx)", func(t *testing.T) {
+		t.Parallel()
+
+		c := jwk.NewClient()
+		ctx, cancel := context.WithCancel(context.Background())
+		cancel()
+		_, err := c.GetJWKSet(ctx, jwksURI)
+		if err == nil {
+			t.Errorf("err == nil")
+		}
+		expect := `context canceled`
+		if !strings.Contains(err.Error(), expect) {
+			t.Errorf("expect != err: (%v) != (%v)", expect, err)
+		}
+	})
+}
+
+func TestGetJSONWebKey(t *testing.T) {
+	t.Parallel()
+
+	mux := http.NewServeMux()
+	testKid := "testKid"
+	mux.HandleFunc("/certs", func(w http.ResponseWriter, r *http.Request) {
+		const format = `{"keys":[{"kty":"RSA","kid":"%s","n":"%s"},{"alg":"RS256","kty":"RSA","n":"z8PS6saDU3h5ZbQb3Lwl_Arwgu65ECMi79KUlzx4tqk8bgxtaaHcqyvWqVdsA9H6Q2ZtQhBZivqV4Jg0HoPHcEwv46SEziFQNR2LH86e-WIDI5pk2NKg_9cFMee9Mz7f_NSQJ3uyD1pu86bdUTYhCw57DbEVDOuubClNMUV456dWx7dx5W4kdcQe63vGg9LXQ-9PPz9AL-0ZKr8eQEHp4KRfRUfngjqjYBMTFuuo38l94KR99B04Z-FboGnqYLgNxctwZ9eXbCerb9bV5-Q9Gb3zoo0x1h90tFdgmC2ZU1xcIIjHmFqJ29mSDZHYAAYtMNAeWreK4gqWJunc9o0vpQ","use":"sig","kid":"713fd68c966e29380981edc0164a2f6c06c5702a","e":"AQAB"},{"kty":"RSA","e":"AQAB","alg":"RS256","use":"sig","kid":"27b86dc6938dc327b204333a250ebb43b32e4b3c","n":"1X7rNtYVglDjBJgsBOSv7C6MYU6Mv-yraGOp_AGs777c2UcVGj88dBe9KihGicQ3LqU8Vf5fVhPixVy0GFBS7mJt3qJryyBpmG7sChnJQBwJmZEffZUl_rLtwGli8chbZj_Fpgjd-7t74VQJmn2SYkFqHNB3vrW_I8zmwn7_Enn4N84d4dP5R9UChUSLhuPNKaKA-a4vtTKy1LNoZpbr6LG1_QaWGDKNhgPWR-6l5fmBdaXtUgDmPFwdQZuiBUDfnPQ7t1lSUD2PJMnG3M9DKG5gqpSk1L1AlWsxntideNsKWIviZ5PhCpmzEComWNtFtFrzfAWQvLkBbgb0pwWp5w"}]}`
+		_, _ = w.Write([]byte(fmt.Sprintf(format, testKid, time.Now().Format(time.RFC3339Nano))))
+	})
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte(""))
+	})
+	s := httptest.NewServer(mux)
+	jwksURI := fmt.Sprintf("http://%s/certs", s.Listener.Addr().String())
+
+	jwks, err := jwk.GetJWKSet(context.Background(), jwksURI)
+	if err != nil {
+		t.Errorf("err != nil: %v", err)
+	}
+
+	t.Run("success()", func(t *testing.T) {
+		t.Parallel()
+
+		_, err := jwks.GetJSONWebKey(testKid)
+		if err != nil {
+			t.Errorf("err != nil: %v", err)
+		}
+	})
+
+	t.Run("failure()", func(t *testing.T) {
+		t.Parallel()
+
+		_, err := jwks.GetJSONWebKey("no_such_key_id")
+		if err == nil {
+			t.Errorf("err == nil")
+		}
+		expect := jwk.ErrKidNotFound.Error()
+		if !strings.Contains(err.Error(), expect) {
+			t.Errorf("expect != err: (%v) != (%v)", expect, err)
+		}
+	})
+}
