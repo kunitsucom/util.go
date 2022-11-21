e2a62be481c72b968032e4ddc8568045b444ad77
ginokent <29125616+ginokent@users.noreply.github.com>
2022-11-21 12:28:51 +0900

    feat: add jose package

---
+++ pkg/jose/jose.go
@@ -0,0 +1,30 @@
+package jose
+
+// ref. JOSE Header https://datatracker.ietf.org/doc/html/rfc7515#section-4
+type Header struct {
+	// ref. https://datatracker.ietf.org/doc/html/rfc7515#section-4.1.1
+	Algorithm string `json:"alg"`
+	// ref. https://datatracker.ietf.org/doc/html/rfc7515#section-4.1.2
+	JwksURL string `json:"jku,omitempty"`
+	// ref. https://datatracker.ietf.org/doc/html/rfc7515#section-4.1.3
+	JSONWebKey string `json:"jwk,omitempty"`
+	// ref. https://datatracker.ietf.org/doc/html/rfc7515#section-4.1.4
+	KeyID string `json:"kid,omitempty"`
+	// ref. https://datatracker.ietf.org/doc/html/rfc7515#section-4.1.5
+	X509URL string `json:"x5u,omitempty"`
+	// ref. https://datatracker.ietf.org/doc/html/rfc7515#section-4.1.6
+	X509CertificateChain string `json:"x5c,omitempty"`
+	// ref. https://datatracker.ietf.org/doc/html/rfc7515#section-4.1.7
+	X509CertificateSHA1Thumbprint string `json:"x5t,omitempty"`
+	// ref. https://datatracker.ietf.org/doc/html/rfc7515#section-4.1.8
+	X509CertificateSHA256Thumbprint string `json:"x5t#S256,omitempty"` //nolint:tagliatelle
+	// ref. https://datatracker.ietf.org/doc/html/rfc7515#section-4.1.9
+	Type string `json:"typ,omitempty"`
+	// ref. https://datatracker.ietf.org/doc/html/rfc7515#section-4.1.10
+	ContentType string `json:"cty,omitempty"`
+	// ref. https://datatracker.ietf.org/doc/html/rfc7515#section-4.1.11
+	Critical string `json:"crit,omitempty"`
+
+	// ref. https://datatracker.ietf.org/doc/html/rfc7515#section-4.3
+	PrivateHeaderParameters map[string]string `json:"-"`
+}
