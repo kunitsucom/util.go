72255684b49071aa5211fa37ac3cd8ee648e8304
ginokent <29125616+ginokent@users.noreply.github.com>
2023-02-24 08:07:50 +0900

    feat: add zipz (archive/zip) package

---
+++ pkg/archive/zip/zip.go
@@ -0,0 +1,67 @@
+package zipz
+
+import (
+	"archive/zip"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var ErrDoNotUnzipAFileAtRiskOfZipSlip = errors.New("zipz: do not unzip a file at risk of zip slip")
+
+func Unzip(srcZipFilePath, dstDir string) error {
+	r, err := zip.OpenReader(srcZipFilePath)
+	if err != nil {
+		return fmt.Errorf("zip.OpenReader: %w", err)
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		if _, err := unzip(f, dstDir); err != nil {
+			return fmt.Errorf("unzip: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func unzip(zipfile *zip.File, dstDir string) (path string, err error) {
+	if strings.Contains(zipfile.Name, "..") {
+		return "", fmt.Errorf("zipfile.Name=%s: %w", zipfile.Name, ErrDoNotUnzipAFileAtRiskOfZipSlip)
+	}
+
+	r, err := zipfile.Open()
+	if err != nil {
+		return "", fmt.Errorf("zipfile.Open: %w", err)
+	}
+	defer r.Close()
+
+	path = filepath.Join(dstDir, filepath.Clean(zipfile.Name))
+
+	if zipfile.FileInfo().IsDir() {
+		if err := os.MkdirAll(path, zipfile.Mode()); err != nil {
+			return "", fmt.Errorf("name=%s: os.MkdirAll: %w", path, err)
+		}
+		return path, nil
+	}
+
+	w, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, zipfile.Mode())
+	if err != nil {
+		return "", fmt.Errorf("name=%s: os.OpenFile: %w", path, err)
+	}
+	defer w.Close()
+
+	for {
+		if _, err := io.CopyN(w, r, 2048); err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return "", fmt.Errorf("name=%s: io.CopyN: %w", path, err)
+		}
+	}
+
+	return path, nil
+}
