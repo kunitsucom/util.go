41660db9f2cbd74612a0164bb00febe0ab0a3b95
ginokent <29125616+ginokent@users.noreply.github.com>
2023-05-15 07:21:57 +0900

    feat: add NotifyContext func in os/signal package

--- pkg/os/signal/signal.go
+++ pkg/os/signal/signal.go
@@ -1,6 +1,7 @@
 package signalz
 
 import (
+	"context"
 	"os"
 	"os/signal"
 )
@@ -9,4 +10,32 @@ func Notify(c chan os.Signal, sig ...os.Signal) chan os.Signal {
 	signal.Notify(c, sig...)
 
 	return c
+}
+
+type ContextSignalHandler func(signal os.Signal, stop context.CancelFunc)
+
+func DefaultContextSignalHandler(_ os.Signal, stop context.CancelFunc) {
+	stop()
+}
+
+func NotifyContext(parent context.Context, handler ContextSignalHandler, signals ...os.Signal) (ctx context.Context, stop context.CancelFunc) {
+	ctx, cancel := context.WithCancel(parent)
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, signals...)
+	if ctx.Err() == nil {
+		go func() {
+			for {
+				select {
+				case sig := <-ch:
+					if handler == nil {
+						handler = DefaultContextSignalHandler
+					}
+					handler(sig, cancel)
+				case <-ctx.Done():
+					return
+				}
+			}
+		}()
+	}
+	return ctx, cancel
 }
