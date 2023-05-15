9541393705dd3da83e94a95e10f6bbd8d91fdd9d
ginokent <29125616+ginokent@users.noreply.github.com>
2023-05-16 00:07:16 +0900

    test: add os/signal package test

--- pkg/os/signal/signal.go
+++ pkg/os/signal/signal.go
@@ -12,15 +12,19 @@ func Notify(c chan os.Signal, sig ...os.Signal) chan os.Signal {
 	return c
 }
 
-type ContextSignalHandler func(signal os.Signal, stop context.CancelFunc)
+type ContextSignalHandler func(signal os.Signal, stop context.CancelCauseFunc)
 
-func DefaultContextSignalHandler(_ os.Signal, stop context.CancelFunc) {
-	stop()
+func DefaultContextSignalHandler(_ os.Signal, stop context.CancelCauseFunc) {
+	stop(context.Canceled)
 }
 
-func NotifyContext(parent context.Context, handler ContextSignalHandler, signals ...os.Signal) (ctx context.Context, stop context.CancelFunc) {
-	ctx, cancel := context.WithCancel(parent)
+func NotifyContext(parent context.Context, handler ContextSignalHandler, signals ...os.Signal) (ctx context.Context, stop context.CancelCauseFunc) {
 	ch := make(chan os.Signal, 1)
+	return notifyContext(parent, ch, handler, signals...)
+}
+
+func notifyContext(parent context.Context, ch chan os.Signal, handler ContextSignalHandler, signals ...os.Signal) (ctx context.Context, stop context.CancelCauseFunc) {
+	ctx, cancel := context.WithCancelCause(parent)
 	signal.Notify(ch, signals...)
 	if ctx.Err() == nil {
 		go func() {
--- pkg/os/signal/signal_test.go
+++ pkg/os/signal/signal_test.go
@@ -1,15 +1,61 @@
-package signalz_test
+package signalz //nolint:testpackage
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"os"
 	"testing"
-
-	signalz "github.com/kunitsuinc/util.go/pkg/os/signal"
+	"time"
 )
 
 func TestNotify(t *testing.T) {
 	t.Parallel()
 	source := make(chan os.Signal, 1)
-	c := signalz.Notify(source, os.Interrupt)
+	c := Notify(source, os.Interrupt)
 	close(c)
+}
+
+func TestNotifyContext(t *testing.T) {
+	t.Parallel()
+	t.Run("success()", func(t *testing.T) {
+		t.Parallel()
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		ctx, stop := NotifyContext(ctx, nil, os.Interrupt)
+		stop(nil)
+		if !errors.Is(ctx.Err(), context.Canceled) {
+			t.Errorf("actual != expect: %v != %v", ctx.Err(), context.Canceled)
+		}
+	})
+}
+
+func Test_notifyContext(t *testing.T) {
+	t.Parallel()
+	t.Run("success(DefaultContextSignalHandler)", func(t *testing.T) {
+		t.Parallel()
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		ch := make(chan os.Signal, 1)
+		ctx, _ = notifyContext(ctx, ch, nil, os.Interrupt)
+		ch <- os.Interrupt
+		<-ctx.Done()
+		if err := context.Cause(ctx); !errors.Is(err, context.Canceled) {
+			t.Errorf("actual != expect: %v != %v", err, context.Canceled)
+		}
+	})
+	t.Run("success(CustomContextSignalHandler)", func(t *testing.T) {
+		t.Parallel()
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		ch := make(chan os.Signal, 1)
+		ctx, _ = notifyContext(ctx, ch, func(sig os.Signal, stop context.CancelCauseFunc) {
+			stop(fmt.Errorf("signal=%s: %w", sig, context.Canceled))
+		}, os.Interrupt)
+		ch <- os.Interrupt
+		<-ctx.Done()
+		if err := context.Cause(ctx); !errors.Is(err, context.Canceled) {
+			t.Errorf("actual != expect: %v != %v", err, context.Canceled)
+		}
+	})
 }
