a630bfcb870fa999d6d46fa4a04974ed2a049b94
ginokent <29125616+ginokent@users.noreply.github.com>
2023-07-17 06:13:59 +0900

    feat: add jws.WithECDSAKeyPEM SigningKeyOption

--- pkg/jose/jws/sign.go
+++ pkg/jose/jws/sign.go
@@ -3,12 +3,15 @@ package jws
 import (
 	"crypto/ecdsa"
 	"crypto/rsa"
+	"fmt"
 
+	x509z "github.com/kunitsuinc/util.go/pkg/crypto/x509"
 	"github.com/kunitsuinc/util.go/pkg/jose/jwa"
 )
 
 type SigningKeyOption struct {
 	key any
+	err error
 }
 
 func WithKey(key any) SigningKeyOption {
@@ -29,6 +32,19 @@ func WithECDSAKey(key *ecdsa.PrivateKey) SigningKeyOption {
 	return WithKey(key)
 }
 
+func WithECDSAKeyPEM(pemBytes []byte) SigningKeyOption {
+	key, err := x509z.ParseECDSAPrivateKeyPEM(pemBytes)
+
+	return SigningKeyOption{
+		key: key,
+		err: fmt.Errorf("x509z.ParseECDSAPrivateKeyPEM: %w", err),
+	}
+}
+
 func Sign(alg string, keyOpt SigningKeyOption, signingInput string) (signatureEncoded string, err error) {
+	if keyOpt.err != nil {
+		return "", fmt.Errorf("keyOpt.err: %w", keyOpt.err)
+	}
+
 	return jwa.JWS(alg).Sign(keyOpt.key, signingInput) //nolint:wrapcheck
 }
