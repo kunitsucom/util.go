3d739a49e15986df349bad888981efcdd68eb864
ginokent <29125616+ginokent@users.noreply.github.com>
2023-07-17 17:48:49 +0900

    fix: lint

--- pkg/net/url/url_test.go
+++ pkg/net/url/url_test.go
@@ -7,21 +7,20 @@ import (
 )
 
 func TestNewValues(t *testing.T) {
+	t.Parallel()
+
 	const expect = "AddKey1=AddValue1&AddKey2=AddValue2&AddKey3=AddValue3&SetKey1=SetValue1&SetKey2=SetValue2"
 
-	v := urlz.NewValues(
+	actual := urlz.NewValues(
 		urlz.Add("AddKey1", "AddValue1"),
 		urlz.Add("AddKey2", "AddValue2"),
 		urlz.Add("SetKey1", "BeforeValue"),
 		urlz.Set("SetKey1", "SetValue1"),
 		urlz.Add("AddKey3", "AddValue3"),
 		urlz.Set("SetKey2", "SetValue2"),
-	)
-
-	actual := v.Encode()
+	).Encode()
 
 	if actual != expect {
 		t.Errorf("❌: expect != actual: %s != %s", expect, actual)
 	}
-
 }
