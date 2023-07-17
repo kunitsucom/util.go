e581111ad10cec0f3b6614695bbe07651110574c
ginokent <29125616+ginokent@users.noreply.github.com>
2023-07-17 18:59:52 +0900

    feat(net/http): add NewHeader func

--- pkg/net/url/url_test.go
+++ pkg/net/url/url_test.go
@@ -9,18 +9,22 @@ import (
 func TestNewValues(t *testing.T) {
 	t.Parallel()
 
-	const expect = "AddKey1=AddValue1&AddKey2=AddValue2&AddKey3=AddValue3&SetKey1=SetValue1&SetKey2=SetValue2"
+	t.Run("normal", func(t *testing.T) {
+		t.Parallel()
 
-	actual := urlz.NewValues(
-		urlz.Add("AddKey1", "AddValue1"),
-		urlz.Add("AddKey2", "AddValue2"),
-		urlz.Add("SetKey1", "BeforeValue"),
-		urlz.Set("SetKey1", "SetValue1"),
-		urlz.Add("AddKey3", "AddValue3"),
-		urlz.Set("SetKey2", "SetValue2"),
-	).Encode()
+		const expect = "AddKey1=AddValue1&AddKey2=AddValue2&AddKey3=AddValue3&SetKey1=SetValue1&SetKey2=SetValue2"
 
-	if actual != expect {
-		t.Errorf("❌: expect != actual: %s != %s", expect, actual)
-	}
+		actual := urlz.NewValues(
+			urlz.Add("AddKey1", "AddValue1"),
+			urlz.Add("AddKey2", "AddValue2"),
+			urlz.Add("SetKey1", "BeforeValue"),
+			urlz.Set("SetKey1", "SetValue1"),
+			urlz.Add("AddKey3", "AddValue3"),
+			urlz.Set("SetKey2", "SetValue2"),
+		).Encode()
+
+		if actual != expect {
+			t.Errorf("❌: expect != actual: %s != %s", expect, actual)
+		}
+	})
 }
