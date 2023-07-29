4c388bee831f12996142363a72eadc2e61e3fe22
ginokent <29125616+ginokent@users.noreply.github.com>
2023-07-30 02:24:21 +0900

    feat(net/http): add httpz.NewHeaderBuilder (#70)

--- pkg/net/http/header.go
+++ pkg/net/http/header.go
@@ -2,26 +2,42 @@ package httpz
 
 import "net/http"
 
-type Header func(http.Header)
+type HeaderBuilder interface {
+	Add(key, value string) HeaderBuilder
+	Set(key, value string) HeaderBuilder
+	Merge(header http.Header) HeaderBuilder
+	Build() http.Header
+}
 
-func Add(key, value string) func(http.Header) {
-	return func(h http.Header) {
-		h.Add(key, value)
+type headerBuilder struct {
+	header http.Header
+}
+
+func NewHeaderBuilder() HeaderBuilder { //nolint:ireturn
+	return &headerBuilder{
+		header: make(http.Header),
 	}
 }
 
-func Set(key, value string) func(http.Header) {
-	return func(h http.Header) {
-		h.Set(key, value)
-	}
-}
-
-func NewHeader(headers ...Header) http.Header {
-	h := make(http.Header)
-
-	for _, header := range headers {
-		header(h)
-	}
-
+func (h *headerBuilder) Add(key, value string) HeaderBuilder { //nolint:ireturn
+	h.header.Add(key, value)
 	return h
+}
+
+func (h *headerBuilder) Set(key, value string) HeaderBuilder { //nolint:ireturn
+	h.header.Set(key, value)
+	return h
+}
+
+func (h *headerBuilder) Merge(header http.Header) HeaderBuilder { //nolint:ireturn
+	for key, values := range header {
+		for _, value := range values {
+			h.Add(key, value)
+		}
+	}
+	return h
+}
+
+func (h *headerBuilder) Build() http.Header {
+	return h.header
 }
--- pkg/net/http/header_test.go
+++ pkg/net/http/header_test.go
@@ -2,6 +2,7 @@ package httpz_test
 
 import (
 	"bytes"
+	"net/http"
 	"strings"
 	"testing"
 
@@ -14,18 +15,22 @@ func TestNewHeader(t *testing.T) {
 	t.Run("normal", func(t *testing.T) {
 		t.Parallel()
 
-		const expect = "Add-Key-1: AddValue1\\r\\nAdd-Key-2: AddValue2\\r\\nAdd-Key-3: AddValue3\\r\\nSet-Key-1: SetValue1\\r\\nSet-Key-2: SetValue2\\r\\n"
+		const expect = "Add-Key-1: AddValue1\\r\\nAdd-Key-2: AddValue2\\r\\nAdd-Key-3: AddValue3\\r\\nAdd-Key-4: AddValue4\\r\\nSet-Key-1: SetValue1\\r\\nSet-Key-2: SetValue2\\r\\n"
 
 		buf := bytes.NewBuffer(nil)
+		h := http.Header{}
+		h.Add("Add-Key-1", "AddValue1")
 
-		if err := httpz.NewHeader(
-			httpz.Add("Add-Key-1", "AddValue1"),
-			httpz.Add("Add-Key-2", "AddValue2"),
-			httpz.Add("Set-Key-1", "BeforeValue"),
-			httpz.Set("Set-Key-1", "SetValue1"),
-			httpz.Add("Add-Key-3", "AddValue3"),
-			httpz.Set("Set-Key-2", "SetValue2"),
-		).Write(buf); err != nil {
+		if err := httpz.NewHeaderBuilder().
+			Merge(h).
+			Add("Add-Key-2", "AddValue2").
+			Add("Add-Key-3", "AddValue3").
+			Add("Set-Key-1", "BeforeValue").
+			Set("Set-Key-1", "SetValue1").
+			Add("Add-Key-4", "AddValue4").
+			Set("Set-Key-2", "SetValue2").
+			Build().
+			Write(buf); err != nil {
 			t.Errorf("❌: err != nil: %v", err)
 		}
 		actual := strings.ReplaceAll(buf.String(), "\r\n", "\\r\\n")
