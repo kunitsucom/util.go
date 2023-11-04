717644f5675d93e6c7278d413d767faab70e8a88
ginokent <29125616+ginokent@users.noreply.github.com>
2023-11-05 01:58:04 +0900

    chore: fix misc

--- testing/assert/assert.go
+++ testing/assert/assert.go
@@ -7,35 +7,35 @@ import (
 )
 
 // NoError asserts that err is nil.
-func NoError(tb testing.TB, err error) bool {
+func NoError(tb testing.TB, err error) (success bool) {
 	tb.Helper()
 
 	return internal.NoError(tb, tb.Errorf, err)
 }
 
 // Error asserts that err is not nil.
-func Error(tb testing.TB, err error) bool {
+func Error(tb testing.TB, err error) (success bool) {
 	tb.Helper()
 
 	return internal.Error(tb, tb.Errorf, err)
 }
 
 // True asserts that value is true.
-func True(tb testing.TB, value bool) bool {
+func True(tb testing.TB, value bool) (success bool) {
 	tb.Helper()
 
 	return internal.True(tb, tb.Errorf, value)
 }
 
 // Equal asserts that expected and actual are deeply equal.
-func Equal(tb testing.TB, expected, actual interface{}) bool {
+func Equal(tb testing.TB, expected, actual interface{}) (success bool) {
 	tb.Helper()
 
 	return internal.Equal(tb, tb.Errorf, expected, actual)
 }
 
 // NotEqual asserts that expected and actual are not deeply equal.
-func NotEqual(tb testing.TB, expected, actual interface{}) bool {
+func NotEqual(tb testing.TB, expected, actual interface{}) (success bool) {
 	tb.Helper()
 
 	return internal.NotEqual(tb, tb.Errorf, expected, actual)
