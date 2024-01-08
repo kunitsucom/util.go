2d13c3c95d1a5da989f6f56894a8963c45d40c6e
ginokent <29125616+ginokent@users.noreply.github.com>
2024-01-08 15:29:46 +0900

    refactor(exp/database/sql/util/mysql): fix misc in ShowCreateAllTables()

--- exp/database/sql/util/mysql/show_create_all_tables.go
+++ exp/database/sql/util/mysql/show_create_all_tables.go
@@ -39,33 +39,47 @@ func ShowCreateAllTables(ctx context.Context, db sqlQueryerContext, opts ...Show
 		opt.apply(cfg)
 	}
 
+	databaseQuoted := func() string {
+		if cfg.database != "" {
+			return fmt.Sprintf("`%s`", cfg.database)
+		}
+		return "database()"
+	}()
+
 	type TableName struct {
 		TableName string `db:"TABLE_NAME"`
 	}
 
-	q := "SELECT TABLE_NAME FROM information_schema.tables"
-	if cfg.database != "" {
-		q += fmt.Sprintf(" WHERE table_schema = `%s`", cfg.database)
-	} else {
-		q += " WHERE table_schema = database()"
-	}
-
 	tableNames := new([]*TableName)
-	if err := dbz.QueryContext(ctx, tableNames, q); err != nil {
-		return "", errorz.Errorf("dbz.QueryContext: q=%s: %w", q, err)
+	tableNamesQuery := fmt.Sprintf("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = %s", databaseQuoted)
+	if err := dbz.QueryContext(ctx, tableNames, tableNamesQuery); err != nil {
+		return "", errorz.Errorf("dbz.QueryContext: q=%s: %w", tableNamesQuery, err)
 	}
 
-	type CreateStatement struct {
+	// type CreateStatement struct {
+	// 	CreateStatement string `db:"create_statement"`
+	// }
+	type ShowCreateTable struct {
 		TableName       string `db:"Table"`
 		CreateStatement string `db:"Create Table"`
 	}
-	for _, tableName := range *tableNames {
-		createTableStmt := new(CreateStatement)
-		q := fmt.Sprintf("SHOW CREATE TABLE `%s`", tableName.TableName)
-		if err := dbz.QueryContext(ctx, createTableStmt, q); err != nil {
-			return "", errorz.Errorf("dbz.QueryContext: q=%s: %w", q, err)
+	for _, tn := range *tableNames {
+		showCreateTable := new(ShowCreateTable)
+		showCreateTableQuery := fmt.Sprintf("SHOW CREATE TABLE `%s`", tn.TableName)
+		if err := dbz.QueryContext(ctx, showCreateTable, showCreateTableQuery); err != nil {
+			return "", errorz.Errorf("dbz.QueryContext: q=%s: %w", showCreateTableQuery, err)
 		}
-		query += createTableStmt.CreateStatement
+		query += showCreateTable.CreateStatement + ";\n"
+
+		// MEMO: for INDEX
+		// showCreateIndex := fmt.Sprintf("SELECT CONCAT('CREATE INDEX ', INDEX_NAME, ' ON ', TABLE_NAME, ' (', GROUP_CONCAT(COLUMN_NAME ORDER BY SEQ_IN_INDEX), ');') AS 'create_statement' FROM information_schema.STATISTICS WHERE TABLE_SCHEMA = %s AND INDEX_NAME IS NOT NULL AND INDEX_NAME != 'PRIMARY' AND TABLE_NAME = '%s' GROUP BY INDEX_NAME, TABLE_NAME;", databaseQuoted, tn.TableName)
+		// createStatements := new([]*CreateStatement)
+		// if err := dbz.QueryContext(ctx, createStatements, showCreateIndex); err != nil {
+		// 	return "", errorz.Errorf("dbz.QueryContext: q=%s: %w", showCreateIndex, err)
+		// }
+		// for _, createStatement := range *createStatements {
+		// 	query += createStatement.CreateStatement + "\n"
+		// }
 	}
 
 	return query, nil
