bd8438356d4baa4a948d56b31aba90f9346c8afa
ginokent <29125616+ginokent@users.noreply.github.com>
2024-01-12 05:13:39 +0900

    fix: remove exp/database/sql/ddl

--- exp/database/sql/ddl/errors.go
+++
@@ -1,9 +0,0 @@
-package ddl
-
-import "errors"
-
-var (
-	ErrUnexpectedToken = errors.New("unexpected token")
-	ErrNoDifference    = errors.New("no difference")
-	ErrNotSupported    = errors.New("not supported")
-)
